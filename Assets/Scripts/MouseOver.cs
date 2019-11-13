a0ba5d264f396f9072e4a6bcb4fc8bfb1c4352f1
kirill milititksiy <milititksiy3@gmail.com>
2019-11-13 20:47:03 +0200

    Removed And Paste

--- Assets/Scripts/MouseOver.cs
+++ Assets/Scripts/MouseOver.cs
@@ -16,32 +16,19 @@ public class MouseOver : MonoBehaviour
     LineRenderer line;
     GameObject currentTile;
 
-
     public Transform curUnit;
-
-
-  
 
     bool hasPath;
 
     Tile unitTile;
     Tile curTile;
-
     Tile prevTile;
-
-
-
-
-    GridBase grid;
-
-    
 
     Vector3 mousePos;
 
     
 
     
-
 
 
     private void Start()
@@ -54,11 +41,7 @@ public class MouseOver : MonoBehaviour
     public void Init()
     {
         Vector3 worldPos = GridBase.singleton.GetWorldCoordinatesFromTile(0, 1, 0);
-
         curUnit.transform.position = worldPos;
-
-       
-        
 
         GameObject go = new GameObject();
         go.transform.localPosition = new Vector3(0, 1.5f, 0);
@@ -66,7 +49,6 @@ public class MouseOver : MonoBehaviour
         go.name = "move line";
         line = go.AddComponent<LineRenderer>();
         line.material = green;
-
         line.useWorldSpace = false;
         line.startWidth = 0.2f;
         line.endWidth = 0.2f;
@@ -81,7 +63,6 @@ public class MouseOver : MonoBehaviour
     }
 
     private void Update()
-
     {
 
         OnMouseOver();
@@ -92,7 +73,6 @@ public class MouseOver : MonoBehaviour
 
 
 
-    
     void OnMouseOver()
     {
 
@@ -103,112 +83,167 @@ public class MouseOver : MonoBehaviour
 
         
 
-
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         PlayerMove player = GetComponent<PlayerMove>();
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-
         {
-
+            
 
             Tile tile = hit.collider.GetComponent<Tile>();
 
             if (hit.collider.CompareTag("Tiles"))
             {
-                //if (tile.selectable == true)
-                if (tile.visited)
+                if (tile.selectable == true)
+                //if(Input.GetMouseButtonDown(0) && tile.selectable)
                 {
                     tile.selectable = false;
                     tile.hoverOn = true;
                     //tile.target = true;
 
-
-                }
-
-
-
-
-
-
-
-
-                else
-                {
-
-
-
-                    line.positionCount = 0;
                     shortPath.Clear();
+                    Tile next = tile;
+                    while (next != null)
+                    {
+                        shortPath.Add(next);
+                        next = next.parent;
 
-                }
-
-
-
-
-
-                    
-
-
-
-
-
-                
-            }
-
-
-        }
-
-
-        if (player.moving)
-
-        {//Got the trace waypoint to dissappear
-
-            Debug.Log("separator");
-
-            {
-
-                //Debug.Log("separator");
-
-                playerPosList.Clear();
-                for (int i = shortPath.Count - 1; i >= 0; i--)
-                {
-                    shortPath.RemoveAt(i);
+                    }
                     Debug.Log(shortPath.Count);
-                    var zero = Vector3.zero;
-                    line.SetPosition(i, zero);
+                    line.positionCount = shortPath.Count;
+                    
+                    //var j = shortPath.Count;
+                    for (int i = 0; i < shortPath.Count; i++)
+                    {
+                        Debug.Log("hello");
+                        line.SetPosition(i, shortPath[i].transform.position);
+                        
+                    }
+                   
 
                 }
-                //var playerPos = player.transform;
-                //playerPosList.Add(playerPos);
 
-                //Debug.Log(playerPosList[playerPosList.Count -1].transform.position);
 
-                //curTile = GridBase.singleton.GetTileFromWorldPosition(player.transform.position);
 
-                //Debug.Log(curTile.transform.position);
 
-                //Debug.Log(curTile.transform.position);
+
+
+
+
+
+                //else
+                //{
+
+
+                //    line.positionCount = 0;
+                //    shortPath.Clear();
+
+
+                //}
+
+
+                //else 
+                //{
+
+                //var player = GetComponent<PlayerMove>();
+                //if(player.moving == true)
+                //{
+                //    line.positionCount = 0;
+                //    shortPath.Clear();
+                //}
+
+                //if (hit.collider.CompareTag("Player"))
+                //{
+                //    line.positionCount = 0;
+                //    shortPath.Clear();
+                //}
+                //if (tile.target == true)
+                //{
+                //    line.positionCount = 0;
+                //    shortPath.Clear();
+                //}
+                //if(tile.current == true)
+                //{
+                //    line.positionCount = 0;
+                //    shortPath.Clear();
+                //}
 
                 //line.positionCount = 0;
                 //shortPath.Clear();
-                //for (int i = 0; i < shortPath.Count; i++)
-                //{
-                //playerPosList.Add(playerPos);
-                //var l1 = playerPosList;
-                //var ls = shortPath;
-                //if(l1[i].position == shortPath[i].transform.position)
-                //{
-                //    Debug.Log("Commit");
+
                 //}
             }
-            //shortPath.Clear();
-        }
-        //else
-        //{
-        //Debug.Log("not moving");
-        //}
 
+
+        }
+
+
+        //if (Input.GetMouseButtonDown(0))
+        //{   
+        //    //mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //    if(shortPath.Count > 0)
+        //    {
+        //        //Debug.Log(">0");
+        //        //Debug.Log(shortPath.Count);
+        //        //Debug.Log("hello");
+        //        var p = player.transform.position;
+        //        //lineTime += Time.deltaTime;
+        //        var pX = Mathf.RoundToInt(p.x / 2.0f);
+        //        var pY = Mathf.RoundToInt(p.y * 0);
+        //        var pZ = Mathf.RoundToInt(p.z / 2.0f);
+        //        var distance = (mousePos - p).magnitude;
+        //        //var playerTile = GridBase.singleton.GetWorldCoordinatesFromTile(pX, pY, pZ);
+                
+             
+                
+        //    }
+            
+            
+            
+
+
+            
+            
+        //}
+       
     }
-}
+        
     
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
